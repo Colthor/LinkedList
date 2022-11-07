e67795d2f76cfcc8555d554f49ddf790cf1fa962
Michael <Michael@MIMIR>
2022-11-07 00:55:03 +0000

    Add project files.

---
+++ LinkedList/LinkedList.h
@@ -0,0 +1,258 @@
+#pragma once
+#include<memory>
+#include<utility>
+#include<cassert>
+#include<stdexcept>
+
+
+
+template <class T>
+class LinkedList
+{
+template <class T>
+class ListIterator;
+
+public:
+	LinkedList() = default;
+	LinkedList(const LinkedList<T>& other) { *this = other; }
+	LinkedList(LinkedList<T>&& other) { *this = other; }
+	~LinkedList() { empty_list(); }
+
+
+	void push_front(T in);
+	T pop_front();
+	void push_back(T in);
+	T pop_back();
+	size_t size() { return m_size; }
+	void empty_list();
+
+	ListIterator<T> begin() { return ListIterator<T>(this->m_first.get()); };
+	ListIterator<T> end() { return ListIterator<T>(nullptr); };
+
+	inline bool operator==(const LinkedList<T>& other)
+	{
+		if (m_size != other.m_size) return false;
+		Node<T>* lPtr = m_last;
+		Node<T>* rPtr = other.m_last;
+		while (lPtr != nullptr && rPtr != nullptr)
+		{
+			if (lPtr->data != rPtr->data) return false;
+			lPtr = lPtr->prev;
+			rPtr = rPtr->prev;
+		}
+		return true;
+	}
+	inline bool operator!=(const LinkedList<T>& other) { return !(*this == other); };
+
+	inline LinkedList<T>& operator=(const LinkedList<T>& other)
+	{
+		if (*this != other)
+		{
+			empty_list();
+			auto pEnd = other.m_last;
+			while (nullptr != pEnd)
+			{
+				push_front(pEnd->data);
+				pEnd = pEnd->prev;
+			}
+		}
+		return *this;
+	}
+	inline LinkedList<T>& operator=(LinkedList<T>&& other) noexcept
+	{
+		if (*this != other)
+		{
+			empty_list();
+			m_first = std::move(other.m_first);
+			m_last = other.m_last;
+			m_size = other.m_size;
+			assert(other.m_first == nullptr);
+			other.m_last = nullptr;
+			other.m_size = 0;
+		}
+		return *this;
+	}
+
+	
+
+private:
+	
+	template<class T>
+	struct Node
+	{
+		T data{};
+		std::unique_ptr<Node<T>> next {nullptr};
+		Node<T>* prev{nullptr};
+
+		inline bool operator==(const Node<T>& other) { return data == other.data; }
+		inline bool operator!=(const Node<T>& other) { return data != other.data; };
+
+		friend class ListIterator<T>;
+	};
+
+	template<class T>
+	class ListIterator
+	{
+	public:
+		ListIterator() = default;
+		ListIterator(LinkedList::Node<T>* start) :current(start) {}
+		ListIterator(const ListIterator<T>& other) :current(other->current) {}
+
+		inline T& operator*()
+		{
+			if (nullptr != current)
+			{
+				return (current->data);
+			}
+			else
+			{
+				auto err = std::range_error("Dereferenced LinkedList::end()");
+				throw err;
+			}
+		}
+
+		inline bool operator==(const ListIterator<T>& other) { return current == other.current; }
+		inline bool operator!=(const ListIterator<T>& other) { return current != other.current; }
+
+		inline ListIterator<T>& operator++()
+		{
+			if (nullptr != current)
+			{
+				current = current->next.get();
+			}
+			return *this;
+		}
+		inline ListIterator<T>& operator--()
+		{
+			if (nullptr != current)
+			{
+				current = current->prev;
+			}
+			return *this;
+		}
+		ListIterator<T> operator++(int) { auto tmp = *this; ++(*this); return tmp; }
+		ListIterator<T> operator--(int) { auto tmp = *this; --(*this); return tmp; }
+
+	private:
+		LinkedList::Node<T>* current = nullptr;
+	};
+
+	std::unique_ptr<Node<T>> m_first{ nullptr };
+	Node<T>* m_last{ nullptr };
+	size_t m_size{ 0 };
+
+	
+};
+
+
+
+
+
+template <class T>
+void LinkedList<T>::push_front(T in)
+{
+	auto tmp = std::move(m_first);
+	m_first = std::make_unique<Node<T>>();
+	if (nullptr != tmp)
+	{
+		tmp->prev = m_first.get();
+	}
+	else
+	{
+		m_last = m_first.get();
+	}
+	m_first->next = std::move(tmp);
+	m_first->data = in;
+	++m_size;
+	
+}
+
+template <class T>
+T LinkedList<T>::pop_front()
+{
+	if (m_size > 0)
+	{
+		T td = m_first->data;
+		m_first = std::move(m_first->next);
+		if (nullptr != m_first)
+		{
+			m_first->prev = nullptr;
+		}
+		else
+		{
+			m_last = nullptr;
+		}
+		--m_size;
+		return td;
+	}
+	else
+	{
+		std::length_error err("pop_back() called on empty list");
+		throw err;
+	}
+}
+
+template <class T>
+void LinkedList<T>::push_back(T in)
+{
+	std::unique_ptr<Node<T>> tmp;
+	tmp = std::make_unique<Node<T>>();
+	tmp->data = in;
+
+	if (nullptr != m_last)
+	{
+		tmp->prev = m_last;
+		m_last->next = std::move(tmp);
+		m_last = m_last->next.get();
+	}
+	else
+	{
+		m_last = tmp.get();
+		m_first = std::move(tmp);
+	}
+	++m_size;
+	
+}
+
+template <class T>
+T LinkedList<T>::pop_back()
+{
+	if (m_size > 0)
+	{
+		T td = m_last->data;
+		if (nullptr != m_last->prev)
+		{
+			m_last = m_last->prev;
+			m_last->next = nullptr;
+		}
+		else
+		{
+			m_last = nullptr;
+			m_first = nullptr;
+		}
+		--m_size;
+		return td;
+	}
+	else
+	{
+		std::length_error err("pop_back() called on empty list");
+		throw err;
+	}
+}
+
+//prevents stack overflow due to recursive unique_ptr deletions
+template <class T>
+void LinkedList<T>::empty_list()
+{
+	auto pEnd = m_last;
+	while (nullptr != pEnd)
+	{
+		pEnd->next = nullptr;
+		pEnd = pEnd->prev;
+	}
+	m_size = 0;
+	m_last = nullptr;
+	m_first = nullptr;
+}
+
+
---
+++ LinkedList/main.cpp
@@ -0,0 +1,216 @@
+// main.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "LinkedList.h"
+#include <iostream>
+#include <cassert>
+
+// The stack overflow tests are very slow, turn them on/off here.
+const bool CHECK_STACK_OVERFLOW{ false };
+
+void testPushPopFront()
+{
+    LinkedList<int> l;
+    l.push_front(1);
+    l.push_front(2);
+    l.push_front(3);
+    assert(3 == l.pop_front());
+    assert(2 == l.pop_front());
+    assert(1 == l.pop_front());
+    assert(0 == l.size());
+}
+void testPushPopBack()
+{
+    LinkedList<int> l;
+    l.push_back(1);
+    l.push_back(2);
+    l.push_back(3);
+    assert(3 == l.pop_back());
+    assert(2 == l.pop_back());
+    assert(1 == l.pop_back());
+    assert(0 == l.size());
+}
+
+void testPushPopBoth()
+{
+    LinkedList<int> l;
+    l.push_back(1);
+    assert(1 == l.pop_front());
+    assert(0 == l.size());
+
+    l.push_front(3);
+    assert(3 == l.pop_back());
+    assert(0 == l.size());
+
+    l.push_back(1);
+    l.push_back(2);
+    assert(1 == l.pop_front());
+    assert(2 == l.pop_front());
+    assert(0 == l.size());
+
+    l.push_front(1);
+    l.push_front(2);
+    assert(1 == l.pop_back());
+    assert(2 == l.pop_back());
+    assert(0 == l.size());
+
+}
+
+void testListEquality()
+{
+    LinkedList<int> L1, L2;
+
+    assert(L1 == L2);
+
+    L1.push_back(1);
+    assert(L1 != L2);
+
+    L2.push_back(1);
+    assert(L1 == L2);
+
+    L1.push_back(2);
+    assert(L1 != L2);
+
+    L2.push_back(2);
+    assert(L1 == L2);
+
+}
+
+void testListCopy(LinkedList<int> l)
+{
+    assert(5 == l.pop_back());
+    assert(3 == l.pop_back());
+    assert(1 == l.pop_back());
+    assert(1 == l.pop_back());
+    assert(0 == l.size());
+}
+
+void testListCopyAssignment()
+{
+    LinkedList<int> L1, L2;
+
+    assert(&L1 != &L2);
+    assert(L1 == L2);
+
+    L1.push_back(1);
+    L1.push_back(1);
+    L1.push_back(3);
+    L1.push_back(5);
+
+    L2 = L1;
+    assert(&L1 != &L2);
+    assert(L1 == L2);
+    testListCopy(L1);
+    assert(L1 == L2);
+    L2 = L2;
+    assert(L1 == L2);
+    L2 = L1;
+    L1.push_back(1);
+    L1.push_back(1);
+    L1.push_back(3);
+    L1.push_back(5);
+    assert(L1 != L2);
+
+
+    if (CHECK_STACK_OVERFLOW)
+    {
+        //check stack overflow on assignment
+        for (auto i{ 0 }; i < 10000000; ++i)
+        {
+            L2.push_back(i);
+        }
+        L2 = L1;
+        assert(L1 == L2);
+    }
+
+}
+
+void testDropStackOverflow()
+{
+    if (CHECK_STACK_OVERFLOW)
+    {
+        LinkedList<int> l;
+        for (auto i{ 0 }; i < 10000000; ++i)
+        {
+            l.push_back(i);
+        }
+    }
+}
+
+void testListMove()
+{
+    LinkedList<int> L1, L2, L3;
+
+    L1.push_back(1);
+    L1.push_back(1);
+    L1.push_back(3);
+    L1.push_back(5);
+
+    L2 = L1;
+    assert(L1 == L2);
+    L3 = std::move(L2);
+    assert(L1 == L3);
+    assert(0 == L2.size());
+
+}
+
+void testIterator()
+{
+    LinkedList<int> l;
+    l.push_back(1);
+    l.push_back(2);
+    l.push_back(3);
+    
+    auto it = l.begin();
+    assert(1 == *it);
+    ++it;
+    assert(2 == *it);
+    ++it;
+    assert(3 == *it);
+    ++it;
+    assert(l.end() == it);
+
+}
+
+void testRangeFor()
+{
+    LinkedList<int> l;
+    l.push_back(1);
+    l.push_back(2);
+    l.push_back(3);
+
+    int t = 1;
+    for (auto& i : l)
+    {
+        assert(i == t);
+        ++t;
+    }
+
+
+}
+
+int main()
+{
+    testPushPopFront();
+    testPushPopBack();
+    testPushPopBoth();
+    testListEquality();
+    testListCopyAssignment();
+    testDropStackOverflow();
+    testListMove();
+    testIterator();
+    testRangeFor();
+
+    std::cout << "All tests done." << std::endl;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
